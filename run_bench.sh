f8d0259ba1b2406dc1213e6c8b9f5af9612b7675
ahbao <www25130101232@gmail.com>
2022-11-21 12:16:54 +0800

    run test bench

---
+++ run_bench.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+thread_num=(1 2 3 4 8 16 24 32)
+FILE="./MT_matrix"
+PATH_BENCH="Test_case_1"
+KEY_WORD="real"
+RESULT_FILE="record_time.txt"
+
+rm $RESULT_FILE
+touch $RESULT_FILE
+
+#read -s -p "Enter Password for sudo: " sudoPW
+#echo "good"
+#echo $sudoPW | sudo make unload
+#make clean
+#make all
+
+make mt
+touch tmp
+echo "--------------------------"
+echo "Start running test case"
+for j in ${PATH_BENCH}
+do
+    echo "====${j}====" | tee -a $RESULT_FILE
+    for i in ${thread_num[@]}
+    do
+        echo "--> running in $i num of thread" | tee -a $RESULT_FILE
+		{ time $FILE $i $PATH_BENCH/m1.txt $PATH_BENCH/m2.txt ; } 2> tmp 
+		cat tmp | grep $KEY_WORD | tee -a $RESULT_FILE
+    done
+    
+done
+rm tmp
