d418cf2c58e7a510b0776cc140745d82d80d53d2
ahbao <www25130101232@gmail.com>
2022-11-23 22:04:55 +0800

    solve module prob

--- run_bench.sh
+++ run_bench.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 thread_num=(1 2 3 4 8 16 24 32)
 FILE="./MT_matrix"
-PATH_BENCH="Test_case_*"
+PATH_BENCH="Test_case_3"
 KEY_WORD="real"
 RESULT_FILE="record_time.txt"
 
 rm $RESULT_FILE
 touch $RESULT_FILE
-
-#read -s -p "Enter Password for sudo: " sudoPW
-#echo "good"
-#echo $sudoPW | sudo make unload
-#make clean
-#make all
-
-make mt
+sudo make unload
+make clean
+make all
+gcc -o MT_matrix MT_matrix.c -lpthread
+rm tmp
 touch tmp
 echo "--------------------------"
 echo "Start running test case"
@@ -23,10 +20,12 @@ do
     echo "====${j}====" | tee -a $RESULT_FILE
     for i in ${thread_num[@]}
     do
+        make load
         echo "--> running in $i num of thread" | tee -a $RESULT_FILE
-		{ time $FILE $i $j/m1.txt $j/m2.txt ; } 2> tmp 
-		cat tmp | grep $KEY_WORD | tee -a $RESULT_FILE
+                { time $FILE $i $j/m1.txt $j/m2.txt ; } 2> tmp
+                cat tmp | grep $KEY_WORD | tee -a $RESULT_FILE
+        make unload
     done
-    
+
 done
 rm tmp
